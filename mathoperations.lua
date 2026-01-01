4d2c5a5b40439be6fc00578ec39437a5b3f6467c
npc-gnu <root_4rch@proton.me>
2026-01-01 22:06:28 +0300

    Added mathoperations.lua .

---
+++ mathoperations.lua
@@ -0,0 +1,6 @@
+local a = 8.2
+local b = 4.3
+-- I didn't done basic operation. Anyone that went to 1st grade can do them.
+print(a % b,"% operator")
+print(a / b,"float\n" .. a // b,"integer")
+print(b ^ a,"exponentiation")
